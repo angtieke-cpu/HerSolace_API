554358936e85213827e61ad16a247cedf8ffd08b
angtieke@gmail.com <ashywin99@gmail.com>
2026-03-07 17:05:04 +0530

    Code fixes

--- src/routes/journey.routes.js
+++ src/routes/journey.routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { authenticate } = require("../middlewares/auth.middleware");
 const { saveJourneyDetails } = require("../controllers/journey.controller");
-const { saveJourneyDetails } = require("../controllers/journey.controller");
 
 router.post("/details", authenticate, saveJourneyDetails);
 // router.get("/get", authenticate, get);
