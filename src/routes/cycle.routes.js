ed55e61832006368be3f321b9e950b4a5fd77fc2
angtieke@gmail.com <ashywin99@gmail.com>
2026-03-07 17:06:37 +0530

    update to get call

--- src/routes/cycle.routes.js
+++ src/routes/cycle.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const cycleController = require("../controllers/cycle.controller");
 
-router.post("/prediction", cycleController.getCyclePrediction);
+router.get("/prediction", cycleController.getCyclePrediction);
 
 module.exports = router;
