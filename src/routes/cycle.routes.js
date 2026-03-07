df429ebe085183cd20540337bbdb6ed4d16d218c
angtieke@gmail.com <ashywin99@gmail.com>
2026-03-07 16:58:00 +0530

    Code update

--- src/routes/cycle.routes.js
+++ src/routes/cycle.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const cycleController = require("../controllers/cycleController");
+const cycleController = require("../controllers/cycle.controller");
 
 router.post("/prediction", cycleController.getCyclePrediction);
 
