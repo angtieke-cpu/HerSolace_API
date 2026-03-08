b06f1640d1c0f5aacbcc906c9eb0249c804cf600
angtieke@gmail.com <ashywin99@gmail.com>
2026-03-08 14:10:52 +0530

    cycle details api added

--- src/routes/journey.routes.js
+++ src/routes/journey.routes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { authenticate } = require("../middlewares/auth.middleware");
 const { saveJourneyDetails } = require("../controllers/journey.controller");
+const { getPreviousCycleDetails } = require("../controllers/journey.controller");
 
 router.post("/details", authenticate, saveJourneyDetails);
-// router.get("/get", authenticate, get);
+router.get("/cycle-details", authenticate, getPreviousCycleDetails);
 
 module.exports = router;
