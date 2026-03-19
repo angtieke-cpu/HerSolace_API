fe62c7b7250f0832050c34aaf16d1c684bef2cb4
angtieke@gmail.com <ashywin99@gmail.com>
2026-03-20 00:52:24 +0530

    code change

--- src/routes/ai.routes.js
+++ src/routes/ai.routes.js
@@ -4,6 +4,6 @@ const aiController = require("../controllers/aiCycle.controller");
 const { authenticate } = require("../middlewares/auth.middleware");
 
 router.get("/cycle-insights",authenticate, aiController.getAiCycleInsights);
-router.get("/user-insights",authenticate, aiController.getAiCycleInsightsWithInput);
+router.post("/user-insights",authenticate, aiController.getAiCycleInsightsWithInput);
 
 module.exports = router;
