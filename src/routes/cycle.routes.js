bde37e3e615b7f1d78b212189db7d9404fa5c420
angtieke@gmail.com <ashywin99@gmail.com>
2026-03-19 11:28:29 +0530

    Code change

--- src/routes/cycle.routes.js
+++ src/routes/cycle.routes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { authenticate } = require("../middlewares/auth.middleware");
-const cycleController = require("../controllers/cycle.controller");
+const {getCyclePrediction} = require("../controllers/cycle.controller");
 const { getPreviousCycleDetails } = require("../controllers/cycle.controller");
 const { logLatestPeriod } = require("../controllers/cycle.controller");
 const { getCycleHormoneData } = require("../controllers/cycle.controller");
 const { createDailyLog } = require("../controllers/cycle.controller");
 const { getTodayLog } = require("../controllers/cycle.controller");
 
-router.get("/prediction",authenticate, cycleController.getCyclePrediction);
+router.get("/prediction",authenticate, getCyclePrediction);
 router.get("/cycle-details", authenticate, getPreviousCycleDetails);
 router.post("/period-date", authenticate, logLatestPeriod);
 router.get("/cycle-hormones", authenticate, getCycleHormoneData);
