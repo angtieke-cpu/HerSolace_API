63656497a061af69d592e264022fc523d2c40dff
angtieke-cpu <angtieke@gmail.com>
2026-03-31 00:19:51 +0530

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,7 +7,7 @@ const { linkUserProfile } = require("../controllers/user.controller");
 const { getLinkedProfiles } = require("../controllers/user.controller");
 const { getUserBymobile } = require("../controllers/user.controller");
 const { getTaggedLinkedUsers } = require("../controllers/user.controller");
-
+const { deleteLinkedUser } = require("../controllers/user.controller");
 
 router.post("/user-details", authenticate, updateUserProfile);
 router.get("/user-details", authenticate, getUserProfile);
@@ -15,5 +15,6 @@ router.post("/link-user", authenticate, linkUserProfile);
 router.get("/linked-users", authenticate, getLinkedProfiles);
 router.post("/available-users", authenticate, getUserBymobile);
 router.get("/shared-users", authenticate, getTaggedLinkedUsers);
+router.get("/unlink-user", authenticate, deleteLinkedUser);
 
 module.exports = router;
