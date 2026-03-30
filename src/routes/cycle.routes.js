3b2dcd1dafcb69b97a75ffa9acd10bee3379b321
angtieke-cpu <angtieke@gmail.com>
2026-03-30 21:48:55 +0530

    Update cycle.routes.js

--- src/routes/cycle.routes.js
+++ src/routes/cycle.routes.js
@@ -8,10 +8,12 @@ const { getCycleHormoneData } = require("../controllers/cycle.controller");
 const { createDailyLog } = require("../controllers/cycle.controller");
 const { getTodayLog } = require("../controllers/cycle.controller");
 const { getUserPeriodLogs } = require("../controllers/cycle.controller");
+const { updateLatestCycleDetails } = require("../controllers/cycle.controller");
 
 router.get("/prediction",authenticate, getCyclePrediction);
 router.get("/cycle-details", authenticate, getPreviousCycleDetails);
 router.post("/period-date", authenticate, logLatestPeriod);
+router.put("/last-period", authenticate, logLatestPeriod);
 router.get("/cycle-hormones", authenticate, getCycleHormoneData);
 router.post("/daily-log", authenticate, createDailyLog);
 router.get("/daily-log", authenticate, getTodayLog);
