c44be4bfecc0db0931e3d5b1261f9f90b65d564d
angtieke-cpu <angtieke@gmail.com>
2026-06-24 00:11:24 +0530

    Update ai.routes.js

--- src/routes/ai.routes.js
+++ src/routes/ai.routes.js
@@ -5,5 +5,6 @@ const { authenticate } = require("../middlewares/auth.middleware");
 
 router.get("/cycle-insights",authenticate, aiController.getAiCycleInsights);
 router.post("/user-insights",authenticate, aiController.getAiCycleInsightsWithInput);
+router.post("/user-plan",authenticate, aiController.getCycleTripPlannerInsights);
 
 module.exports = router;
