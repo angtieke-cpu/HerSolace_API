d19bb40b1a417d2467ee5d45e5e089fbc5e061d4
angtieke-cpu <angtieke@gmail.com>
2026-08-19 01:35:59 +0530

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,9 @@ const db = require('../db');
 const { generateToken } = require('../utils/jwt');
 const { sendOtpSms } = require('../utils/twilio');
 const { OAuth2Client } = require("google-auth-library");
+const { Expo } = require("expo-server-sdk");
+
+const expo = new Expo();
 
 const googleClient = new OAuth2Client(
   process.env.GOOGLE_CLIENT_ID
@@ -571,5 +574,32 @@ exports.facebookLogin = async (req, res) => {
 
   } finally {
     client.release();
+  }
+};
+exports.sendPushNotification = async (pushToken, title, body, data = {}) => {
+  try {
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.log("Invalid Expo push token:", pushToken);
+      return;
+    }
+
+    const messages = [
+      {
+        to: pushToken,
+        sound: "default",
+        title,
+        body,
+        data,
+      },
+    ];
+
+    const chunks = expo.chunkPushNotifications(messages);
+
+    for (const chunk of chunks) {
+      const tickets = await expo.sendPushNotificationsAsync(chunk);
+      console.log("Notification tickets:", tickets);
+    }
+  } catch (error) {
+    console.error("Push notification error:", error);
   }
 };
